64493fe9da50f44abb83e460ca6dc7649d1100dd
weiweiluo <weiwei.luo@ijinzhuan.com>
2018-03-06 13:04:58 +0800

    update

--- web/src/main/java/com/logaa/config/WebMvcConfig.java
+++ web/src/main/java/com/logaa/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("/index");
-		registry.addViewController("/index").setViewName("/index");
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/index").setViewName("index");
 	}
 }
