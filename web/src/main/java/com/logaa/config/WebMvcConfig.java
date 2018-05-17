1e1d08a2febcee6f598cb9ac6f5951c8becc3bf4
weiweiluo <weiwei.luo@ijinzhuan.com>
2018-05-17 14:32:25 +0800

    UPDATE

--- web/src/main/java/com/logaa/config/WebMvcConfig.java
+++ web/src/main/java/com/logaa/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.logaa.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,5 +12,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
 	public void addViewControllers(ViewControllerRegistry registry) {
 		/*registry.addViewController("/").setViewName("index/index_v0");
 		registry.addViewController("/index").setViewName("index/index_v0");*/
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//registry.addInterceptor(new LongInterceptor()).addPathPatterns("/**");
 	}
 }
